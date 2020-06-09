c4c5847aab49cdbb870c591cb017d23f1da7533b
Tristan Dufault <mario1952live.ca@live.ca>
2020-06-08 23:46:44 -0400

    c

--- Simulation/src/source/Combatant.java
+++ Simulation/src/source/Combatant.java
@@ -9,20 +9,36 @@ public class Combatant {
 	//Attack Values
 	Weapon wep1;
 	
-	//procs
-	int blproc;
-	int cproc;
+	//Defensive procs
+	int blproc;			//Block Proc
 	
-	//Constants
-	int cstr = 1;
-	int cdex = 1;
-	int cedu = 1;
-	int citl = 1;
-	int cwil = 1;
-	int cagi = 1;
-	int cper = 1;
-	int cblproc = 1;
-	int ccproc = 1;
+	//Offensive Procs
+	int cproc;			//Crit Chance
+	
+	
+	//Constant Modifiers//*********************************************
+	
+	//Base Stat Modifiers
+	float cstr = 1;		//Constant Modifier for Strength
+	float cdex = 1;		//Constant Modifier for Dexterity
+	float cedu = 1;		//Constant Modifier for Endurance
+	float citl = 1;		//Constant Modifier for floatelligence
+	float cwil = 1;		//Constant Modifier for Willpower
+	float cagi = 1;		//Constant Modifier for Agility
+	float cper = 1;		//Constant Modifier for Perception
+	
+	//Defensive Modifiers
+	float cblproc = 1;	//Constant Modifier for Block Chance
+	float physDBres = 1;//Constant Modifier for Phyisical Debuff Resistance
+	float mentDBres = 1;//Constant Modifier for Mental Debuff Resistance
+	
+	//Offensive Modifiers
+	float ccproc = 1;	//Constant Modifier for Crit Chance
+	float ccdproc = 1;	//Constant Modifier for Crit Damage
+	float physDBproc = 1;//Constant Modifier for Physical Debuff Infliction Chance
+	
+	
+	//Constant Modifiers//*********************************************
 	
 	//Base Stats
 	int str;
@@ -32,6 +48,8 @@ public class Combatant {
 	int wil;
 	int agi;
 	int per;
+	
+	
 	
 	public Combatant(int Str, int End, int Dex, int Itl, int Wil, int Agi, int Per, Weapon wep)
 	{
